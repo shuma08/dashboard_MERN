4b1a60e9eb5f64789881f49a150c67be692c2ffe
vitaliy <vitaliy.shumskyy@orb-pay.com>
2023-01-18 23:04:54 +0200

    Dashboard

---
+++ README.md
@@ -0,0 +1,12 @@
+Its a demo version of Dashboard 
+with using own BE and mokData of transactions
+It shows work with MongoDB and API calls
+
+to start server :
+go to the folder /server
+and make  npm install => npm run dev
+
+to start a front-end side:
+go to the folder /client
+and make npm install => npm start
+
--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,11 +1,14 @@
+import React from 'react'
 import { DownloadOutlined, Email, PersonAdd, PointOfSale, Traffic } from '@mui/icons-material';
-import { Box, Button, useMediaQuery, useTheme } from '@mui/material'
+import { Box, Button, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid';
 import FlexBetween from 'components/FlexBetween';
 import Header from 'components/Header';
-import React from 'react'
 import { useGetDashboardQuery } from 'redux/state/api';
 import StatBox from "components/StatBox";
 import OverviewChart from 'components/OverviewChart';
+import { Columns } from './columns';
+import BreakdownChart from 'components/BreakdownChart';
 
 const Dashboard = () => {
   const theme = useTheme();
@@ -15,7 +18,8 @@ const Dashboard = () => {
   return (
     <Box m="1.5rem 2.5rem">
       <FlexBetween>
-        <Header title="DASHBOARD" subTitle="Welcome to your dashboard" />
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+
         <Box>
           <Button
             sx={{
@@ -23,7 +27,7 @@ const Dashboard = () => {
               color: theme.palette.background.alt,
               fontSize: "14px",
               fontWeight: "bold",
-              padding: "10px 20px"
+              padding: "10px 20px",
             }}
           >
             <DownloadOutlined sx={{ mr: "10px" }} />
@@ -35,20 +39,23 @@ const Dashboard = () => {
       <Box
         mt="20px"
         display="grid"
-        gridTemplateColumns="repeat(12,1fr)"
+        gridTemplateColumns="repeat(12, 1fr)"
         gridAutoRows="160px"
         gap="20px"
         sx={{
-          "& > div": { gridColumns: isNonMediumScreens ? undefined : "span 12" }
+          "& > div": { gridColumn: isNonMediumScreens ? undefined : "span 12" },
         }}
       >
+        {/* ROW 1 */}
         <StatBox
           title="Total Customers"
           value={data && data.totalCustomers}
           increase="+14%"
           description="Since last month"
           icon={
-            <Email sx={{ color: theme.palette.secondary[300], fontSize: "26px" }} />
+            <Email
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
           }
         />
         <StatBox
@@ -57,7 +64,9 @@ const Dashboard = () => {
           increase="+21%"
           description="Since last month"
           icon={
-            <PointOfSale sx={{ color: theme.palette.secondary[300], fontSize: "26px" }} />
+            <PointOfSale
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
           }
         />
         <Box
@@ -67,17 +76,17 @@ const Dashboard = () => {
           p="1rem"
           borderRadius="0.55rem"
         >
-
           <OverviewChart view="sales" isDashboard={true} />
         </Box>
-
         <StatBox
           title="Monthly Sales"
           value={data && data.thisMonthStats.totalSales}
           increase="+5%"
           description="Since last month"
           icon={
-            <PersonAdd sx={{ color: theme.palette.secondary[300], fontSize: "26px" }} />
+            <PersonAdd
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
           }
         />
         <StatBox
@@ -86,11 +95,70 @@ const Dashboard = () => {
           increase="+43%"
           description="Since last month"
           icon={
-            <Traffic sx={{ color: theme.palette.secondary[300], fontSize: "26px" }} />
+            <Traffic
+              sx={{ color: theme.palette.secondary[300], fontSize: "26px" }}
+            />
           }
         />
-      </Box>
 
+        {/* ROW 2 */}
+        <Box
+          gridColumn="span 8"
+          gridRow="span 3"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+              borderRadius: "5rem",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: theme.palette.background.alt,
+            },
+            "& .MuiDataGrid-footerContainer": {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderTop: "none",
+            },
+            "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+              color: `${theme.palette.secondary[200]} !important`,
+            },
+          }}
+        >
+          <DataGrid
+            loading={isLoading || !data}
+            getRowId={(row) => row._id}
+            rows={(data && data.transactions) || []}
+            columns={Columns}
+          />
+        </Box>
+        <Box
+          gridColumn="span 4"
+          gridRow="span 3"
+          backgroundColor={theme.palette.background.alt}
+          p="1.5rem"
+          borderRadius="0.55rem"
+        >
+          <Typography variant="h6" sx={{ color: theme.palette.secondary[100] }}>
+            Sales By Category
+          </Typography>
+          <BreakdownChart isDashboard={true} />
+          <Typography
+            p="0 0.6rem"
+            fontSize="0.8rem"
+            sx={{ color: theme.palette.secondary[200] }}
+          >
+            Breakdown of real states and information via category for revenue
+            made for this year and total sales.
+          </Typography>
+        </Box>
+      </Box>
     </Box>
   )
 }
